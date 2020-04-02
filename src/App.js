375283febea92de015dca087269ab27a4bb50254
marlonrodler <marlon.rodler@hotmail.com>
2020-04-02 09:58:35 -0300

    Inserção curso Rocketseat Huntweb

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
 
-function App() {
-  return (
+import './styles.css';
+
+import Header from './components/Header';
+import Main   from './pages/main';
+
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes />
     </div>
-  );
-}
+);
+
 
 export default App;
